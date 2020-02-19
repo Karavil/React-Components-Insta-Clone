94d4e27462af80cb1cbdfce9763a373266e9c8ff
Alex Karavil <alex@karavil.io>
2020-02-18 19:07:45 -0500

    Completed all features

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -4,15 +4,21 @@ import React, { useState } from 'react';
 const CommentInput = props => {
    const [comment, setComment] = useState('');
 
+   const submitComment = (e) => {
+      e.preventDefault();
+      props.submitComment(comment);
+   }
+
+   const commentChange = (e) => {
+      setComment(e.target.value);
+   }
+
    return (
-      <form className="comment-form" onSubmit={(e) => {
-         e.preventDefault();
-         props.submitComment(comment);
-      }}>
+      <form className="comment-form" onSubmit={submitComment}>
          <input
             type="text"
             placeholder="Add comment... "
-            onChange={(event) => setComment(event.target.value)}
+            onChange={commentChange}
          />
       </form>
    );
